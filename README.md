ccf6c726a96f817f4e557e0770e99214d95f6ec8
CAMILA ROCHA OLIVEIRA <camila6045@digitalcollegesul.local>
2024-11-06 15:59:18 -0300

    feat

--- README.md
+++ README.md
@@ -16,3 +16,27 @@ Legenda:
 4 - (CR, CP) O professor endoidou e disse que agora haverá provas teóricas e práticas por módulo, sendo 1 teórica e 2 práticas, o devaneio foi tão grande que ele disse que a prova teórica terá peso 2  e a prática peso 1 ou seja, duas provas práticas valem o mesmo que uma prova teórica, sabendo disso crie uma função que receba as tres notas e devolva a média;
 
 5 - (SR, CP) Na Digital College os professores que possuem carro tem direito a um cartão estacionamento para guardarem seus veículos, quando um professor passa pela cancela, a mesma registra o horário em que ele entrou ou saiu, o gerente quer saber em que período do dia os professores mais acessam o estacionamento, sabendo disso crie uma função que identifique pelo horário em que período do dia(Manhã(05:00 as 11:59), Tarde(12:00 as 17:59), ou Noite(18:00 as 04:59)) aconteceu o acesso, execute essa função 10 vezes com horários diferentes, e escreva na tela a quantidade de acessos de cada período;
+
+6- crie uma função que retorne a quantidade de itens de um array (não pode usar length)
+
+7- crie uma função que retorne todos os números pares deste array [1,2,3,4,5,6,7,8,9,10]
+(não pode usar filter nem length)
+
+8- crie uma função que transforme frases de snake_case para camelCase
+(não pode usar replace,split,join)
+
+9- crie uma função que retorne "true" caso todos os itens de um array obedecerem a uma condição (não pode usar map, every)
+
+10- crie uma função que retorne o inverso de uma string (não pode usar o reverse)
+
+11- Faça uma função em que verifica se a senha do usuario verifique a força da senha:
+- Letra maiuscula
+- numero
+- carctere especial
+- minimo 8 careteres (obrigatorio)
+
+forte: 4 requisitos
+medio: pelo menos 3 requisitos
+fraca: 2 requisitos
+muito fraca: 1 requisito
+invalida: 0 requisitos
--- js/exercicio.js
+++ js/exercicio.js
@@ -1,74 +1,155 @@
 //1)
-function boasVindas(){
+function boasVindas() {
     document.write(`Seja bem-vindo thiago!`)
 }
 boasVindas();
 
 //2)
-function calcIdade(ano,mes){
-    document.write(`<h1>a idade de thiago  é ${2024 - ano} ${mes == 'mes' ?'x':'y'}</h1>`)
+function calcIdade(ano, mes) {
+    document.write(`<h1>a idade de thiago  é ${2024 - ano} ${mes == 'mes' ? 'x' : 'y'}</h1>`)
 }
-calcIdade(1993,12)
+calcIdade(1993, 12)
 
 //3)
-function frequenciaAluno (presentes, total){
-return (((total-presentes))*100/total).toFixed(0);
+function frequenciaAluno(presentes, total) {
+    return (((total - presentes)) * 100 / total).toFixed(0);
 }
 
-document.write(`<h1>O percentual de faltantes é: ${frequenciaAluno(10,12)}
+document.write(`<h1>O percentual de faltantes é: ${frequenciaAluno(11, 19)}
 % </h1>`)
 
 //4)
 
-function calcularMedia(notaTeorica, notaPratica1, notaPratica2){
-     return ((notaTeorica *2 ) + notaPratica1 + notaPratica2) /4 ;
+function calcularMedia(notaTeorica, notaPratica1, notaPratica2) {
+    return ((notaTeorica * 2) + notaPratica1 + notaPratica2) / 4;
 }
 
-document.write(`<h1>A média é: ${calcularMedia(10,10,10)} </h1>`)
+document.write(`<h1>A média é: ${calcularMedia(10, 10, 10)} </h1>`)
 
-//5)
 function idntPeriodo(horario){
-    const [hora,minutos] = horario.split(':')
-   if(Number(hora) >= 5 && Number(hora) <12) return 'manhã';
-   else if (Number(hora) >=12 && Number (hora) <18) return 'tarde';
-   else return 'noite';
-}  
-
-const horario1 = idntPeriodo('12:30')
-const horario2 = idntPeriodo('10:30')
-const horario3 = idntPeriodo('04:45')
-const horario4 = idntPeriodo('20:00')
-const horario5 = idntPeriodo('21:00')
-const horario6 = idntPeriodo('08:00')
-const horario7 = idntPeriodo('09:00')
-const horario8 = idntPeriodo('03:22')
-const horario9 = idntPeriodo('05:12')
-const horario10 = idntPeriodo('12:30')
-
-   
-
-
-let manha = 0;
-let tarde = 0;
-let noite = 0;
-
-function contagemDePeriodos(horario){
-    if(horario == 'manhã') manha++;
-    else if(horario == 'tarde') tarde++;
-    else noite++;
+    const[hora, minutos] = horario.split(':')
+        if (Number(hora) >= 5 && Number(hora) < 12) return 'manha'
+        else if (Number(hora) >= 12 && Number(hora) < 18) return 'tarde'
+        else return 'noite'
+    
 }
- 
-document.write (`<h1>total de acessos da manhã foram: ${manha} </h1>`)
-document.write (`<h1>total de acessos da tarde foram: ${tarde} </h1>`)
-document.write (`<h1>total de acessos da noite foram: ${noite} </h1>`)
+const horario1 = idntPeriodo ('12:30')
+const horario2 = idntPeriodo ('10:30')
+const horario3 = idntPeriodo ('04:45')
+const horario4 = idntPeriodo ('20:00')
+const horario5 = idntPeriodo ('21:30')
+const horario6 = idntPeriodo ('08:00')
+const horario7 = idntPeriodo ('09:00')
+const horario8 = idntPeriodo ('03:22')
+const horario9 = idntPeriodo ('05:12')
+const horario10 = idntPeriodo ('12:30')
 
-contagemDePeriodos(horario1);
-contagemDePeriodos(horario2);
-contagemDePeriodos(horario3);
-contagemDePeriodos(horario4);
-contagemDePeriodos(horario5);
-contagemDePeriodos(horario6);
-contagemDePeriodos(horario7);
-contagemDePeriodos(horario8);
-contagemDePeriodos(horario9);
-contagemDePeriodos(horario10);
+function contagemDePeriodo(horario){
+    if(horario == 'manha') manha++
+    else if(horario=='tarde') tarde++
+    else noite++
+}
+
+let manha = 0
+let tarde = 0
+let noite = 0
+
+
+contagemDePeriodo(horario1)
+contagemDePeriodo(horario2)
+contagemDePeriodo(horario3)
+contagemDePeriodo(horario4)
+contagemDePeriodo(horario5)
+contagemDePeriodo(horario6)
+contagemDePeriodo(horario7)
+contagemDePeriodo(horario8)
+contagemDePeriodo(horario9)
+contagemDePeriodo(horario10)
+
+
+document.write(`a quantidade de acessos por período foi: <br>
+    <h2>manhã:${manha}</h2> 
+    <h2>tarde:${tarde}</h2> 
+    <h2>noite:${noite}</h2>
+    `)
+
+
+//6)
+function qtdItens(array) {
+    let tamanho = 0;
+
+    while (!!array[tamanho] == true){
+            tamanho++;
+        } 
+    return tamanho;
+}
+document.write(`<h1> O tamanho da palavra Jessica é ${qtdItens('jessica')}</h1>`);
+
+//6.a)
+function itens(array){
+    return array.length;
+
+}
+document.write(`<h1> O tamanho da palavra camila é ${itens('camila')}</h1>`);
+
+//7)
+function soPares(array) {
+    let pares = [];
+    for (let i = 0; i < qtdItens(array); i++) {
+        if (array[i] % 2 == 0) {
+            pares.push(array[i])
+        }
+    }
+    return pares;
+}
+document.write(`<h1> os pares do array [1,2,3,4,5,6,7,8,9,10] são: ${soPares([1,2,3,4,5,6,7,8,9,10])}`)
+
+//7a)
+function par(array){
+    return array.filter((numero)=> numero % 2==0);
+}
+document.write(`<h1> os pares do array [4,8,3,82,10] são: ${par([4,8,3,82,10])}`)
+
+//8)
+function camelCase(frase){
+    let fraseNova = '';
+    for(let i = 0; i < qtdItens(frase); i++){
+        if(frase[i] == '_'){
+            fraseNova += '';
+        } else if(frase[i - 1] == '_'){
+            fraseNova += frase[i].toUpperCase();
+        }else{
+            fraseNova += frase[i];
+        }
+    }
+    return fraseNova;
+}
+
+//8a)
+function camCse(frase){
+    return frase.replace("_", "" ); 
+    frase= letter.toUpperCase(i[0]);
+    
+}
+
+document.write(`<h1>A frase snake_case fica assim "${camCse('snake_case')}" em camelCase</h1>`)
+
+//9)
+function todosVerdadeiros(array, condicao){
+    let isTrue = true;
+    for(let i = 0; i < qtdItens(array); i++){
+        if(!eval(array[i] + condicao)){
+            isTrue = false;
+        }
+    }
+    return isTrue;
+}
+
+document.write(`<h1>Todos os itens do array[2,4,6,8,5] são pares? 
+    ${todosVerdadeiros([2,4,6,8,5], ' % 2 == 0') ? 'Sim' : 'Não'}</h1>`);
+
+//9a)
+function verd(array){
+    array.every(true)
+
+}
